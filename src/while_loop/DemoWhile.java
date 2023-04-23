11cbfcd6972cddc43ed7b1d74e9f553755c0041c
Richa Singh <richasingh30469@gmail.com>
2023-04-23 18:14:36 +0530

    while loop

---
+++ src/while_loop/DemoWhile.java
@@ -0,0 +1,25 @@
+package while_loop;
+
+public class DemoWhile {
+
+    public static void main(String[] args) {
+
+        //Increment while loop
+
+        int a=0;
+        while(a<5)
+        {
+            System.out.println("Hello");
+            a++;
+        }
+
+        //Decrement while loop
+
+        int i=10;
+        while(i>=1)
+        {
+            System.out.println("Hello : "+i);
+            i--;
+        }
+    }
+}
