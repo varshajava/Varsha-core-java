5cd65f74eaccc4eedbcb568780de38b82a4d9361
Richa Singh <richasingh30469@gmail.com>
2023-04-23 22:08:21 +0530

    for loop programs

--- src/for_loop/ForEven.java
+++ src/for_loop/ForEven.java
@@ -1,12 +1,22 @@
 package for_loop;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ForEven {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Even numbers are : ");
 
-        for(int a=1;a<=50;a++)
+
+        InputStreamReader varsha=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(varsha);
+
+        System.out.println("enter any number : ");
+        int n=Integer.parseInt(br.readLine());
+
+        for(int a=1;a<=n;a++)
         {
             if(a%2==0)
             {
