efd14b0270433d365cd9212bc6848def0470bbc2
Richa Singh <richasingh30469@gmail.com>
2023-05-18 21:39:25 +0530

    switch_case

---
+++ src/switch_case/SwitchDayConti.java
@@ -0,0 +1,34 @@
+package switch_case;
+
+public class SwitchDayConti {
+
+    public static void main(String[] args) {
+
+        int day=3;
+
+        switch(day)
+        {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tueday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                //continue;  continue does not wrk in switch case
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+        }
+    }
+}
