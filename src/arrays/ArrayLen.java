1b814999d4021b2fbf81057661500cce0fc10890
Richa Singh <richasingh30469@gmail.com>
2023-05-19 23:13:12 +0530

    array

---
+++ src/arrays/ArrayLen.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class ArrayLen {
+
+
+    public static void main(String[] args) {
+
+        int arr[]={10,20,30,40,50,60,70,80,90};
+        int result=arr.length;
+        System.out.println("length of array is : "+result);
+    }
+}
